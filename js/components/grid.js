80d1243edc530c741aa2f1ae238b3cfac4218db8
selfsame <joseph.parker.1@gmail.com>
2015-05-15 23:22:25 -0400

    js/components/grid.js

--- js/components/grid.js
+++ js/components/grid.js
@@ -3,9 +3,39 @@ C("grid",{
     w:640,
     h:480,
     type: Array(),
+    notfound: false,
     data: false,
+
     init:
     function(c){
       c.data = new c.type(c.w * c.h);
-    }});
+
+
+    },
+
+    get:
+    function(x, y){
+      c = this;
+      idx = (y - 1) * c.w + x;
+      found = c.data[idx];
+      if (found == undefined){return c.notfound;} else {return c.data[idx];} },
+
+    set:
+    function(x, y, v){
+      c = this;
+      idx = (y - 1) * c.w + x;
+      c.data[idx] = v; },
+
+    mapindexed: 
+    function(f){
+      c = this;
+      for(i=0;i<c.data.length;i++){
+        y = parseInt(i/c.w);
+        x = i - (y*c.w);
+        f(c.data[i], x, y);
+      }
+    }
+
+    
+});
 
