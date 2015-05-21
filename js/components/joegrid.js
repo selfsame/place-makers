3901941b0a22085e3db8afb77b068c46b14856c3
selfsame <joseph.parker.1@gmail.com>
2015-05-20 21:11:42 -0400

    e.find database no longer commented out

--- js/components/joegrid.js
+++ js/components/joegrid.js
@@ -7,19 +7,19 @@ C("grid",
    data: false,
    notfound: false,
    get: function(x, y){
-      c = this;
+      var c = this;
       idx = (y - 1) * c.w + x;
       found = c.data[idx];
       if (found == undefined){return c.notfound;} else {return c.data[idx];} },
 
     set: function(x, y, v){
-      c = this;
-      idx = (y - 1) * c.w + x;
+      var c = this;
+      var idx = (y - 1) * c.w + x;
       c.data[idx] = v; },
 
     mapindexed: function(f){
-      c = this;
-      for(i=0;i<c.data.length;i++){
+      var c = this;
+      for(var i=0;i<c.data.length;i++){
         y = parseInt(i/c.w);
         x = i - (y*c.w);
         f(i, x, y); }}},
